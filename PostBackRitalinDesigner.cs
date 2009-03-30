1c494caf34723c10a23d2e5ebda85c4ba95c30d1
Encosia <dave@encosia.com>
2009-03-30 15:15:47 +0000

    Initial

---
+++ PostBackRitalinDesigner.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.UI.Design;
+
+namespace Encosia
+{
+  public class PostBackRitalinDesigner : ControlDesigner
+  {
+    public override string GetDesignTimeHtml()
+    {
+      string template = @"<table style=""border:1px solid #CCCCCC;"" cellspacing=""0"" cellpadding=""0"">
+<tr>
+<td nowrap style=""font:messagebox;background-color:#ffffff;color:#444444;background-position:bottom;background-repeat:repeat-x;padding:4px;""><strong>PostBackRitalin</strong> - {0}</td>
+</tr>
+</table>";
+
+      return string.Format(template, this.ID);
+    }
+  }
+}
