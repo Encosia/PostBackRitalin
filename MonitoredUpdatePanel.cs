9c2e5a8c1ede0bccdcaa11477fac07ab91ba2f62
Encosia <dave@encosia.com>
2009-04-06 02:44:17 +0000

    - Removed some redundant code.

--- MonitoredUpdatePanel.cs
+++ MonitoredUpdatePanel.cs
@@ -8,24 +8,14 @@ namespace Encosia
 {
   public class MonitoredUpdatePanel
   {
-    private string _updatePanelID;
-    private string _waitText;
     private string _waitImage;
 
     [Description("The ID of an UpdatePanel to monitor")]
-    public string UpdatePanelID
-    {
-      get { return _updatePanelID; }
-      set { _updatePanelID = value; }
-    }
+    public string UpdatePanelID { get; set; }
 
     [Localizable(true)]
     [Description("The WaitText to use for Buttons in this monitored UpdatePanel.")]
-    public string WaitText
-    {
-      get { return _waitText; }
-      set { _waitText = value; }
-    }
+    public string WaitText { get; set; }
 
     [UrlProperty]
     [Localizable(true)]
@@ -38,17 +28,10 @@ namespace Encosia
 
     [Description("Should all elements within this UpdatePanel be disabled when it triggers a partial postback?")]
     [DefaultValue(false)]
-    public bool DisableAllElements
-    {
-      get; set;
-    }
+    public bool DisableAllElements { get; set; }
   }
 
   public class MonitoredUpdatePanelCollection : List<MonitoredUpdatePanel>
   {
-      public MonitoredUpdatePanelCollection()
-      {
-
-      }
   }
 }
